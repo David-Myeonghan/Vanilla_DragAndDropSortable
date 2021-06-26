256950de7b3d149cba232d7795994bd93977684b
David MH Ryu <myeonghan12@gmail.com>
2021-06-26 18:34:44 +0900

    mouse position and sort

--- script.js
+++ script.js
@@ -16,7 +16,33 @@ draggables.forEach((draggable) => {
 containers.forEach((container) => {
 	container.addEventListener('dragover', (e) => {
 		e.preventDefault();
+		const afterElement = getDragAfterElement(container, e.clientY);
+		console.log(afterElement);
 		const draggable = document.querySelector('.dragging');
-		container.appendChild(draggable);
+		if (afterElement === null) {
+			container.appendChild(draggable);
+		} else {
+			container.insertBefore(draggable, afterElement);
+		}
 	});
 });
+
+// Make it in order using mouse position.
+function getDragAfterElement(container, y) {
+	const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')];
+
+	return draggableElements.reduce(
+		(closest, child) => {
+			const box = child.getBoundingClientRect();
+			const offset = y - box.top - box.height / 2; // exact center of the box
+			if (offset < 0 && offset > closest.offset) {
+				console.log(offset, child);
+				return { offset: offset, element: child };
+			} else {
+				console.log(closest);
+				return closest;
+			}
+		},
+		{ offset: Number.NEGATIVE_INFINITY }
+	).element;
+}
